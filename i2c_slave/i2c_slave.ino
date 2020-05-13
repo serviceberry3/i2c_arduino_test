e0e5d59ab9d145204597e1b1dce791e2d7c27a35
nodog <noah.weiner@yale.edu>
2020-05-13 16:16:04 -0400

    initial code

---
+++ i2c_slave/i2c_slave.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+int LED = 2;
+
+int x = 0;
+
+void setup() {
+  // Define the LED pin as Output
+  pinMode (LED, OUTPUT);
+  
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9);
+  
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read(); // read one character from the I2C
+  Serial.println(x);
+  //Wire.endTransmission(); // stop transmitting
+}
+
+void loop() {
+  if (x == 1) {
+    digitalWrite(LED, HIGH);
+    delay(200);
+  }
+  
+  if (x == 0) {
+    digitalWrite(LED, LOW);
+    delay(200);
+  }
+}
